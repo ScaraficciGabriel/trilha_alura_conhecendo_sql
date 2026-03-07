1f9cfad2b6a259303c0e4075915c8317081aa831
Gabriel Scaraficci <gabriel.scaraficci@gmail.com>
2026-03-07 10:17:48 -0300

    Add SQL queries with comments for clarity

--- conhecendo_sql/mnm_a1.sql
+++ conhecendo_sql/mnm_a1.sql
@@ -1,3 +1,8 @@
+#1. Liste todos os IDs únicos dos produtos vendidos pela empresa, ou seja, uma lista de produtos distintos. Utilize a cláusula DISTINCT para garantir que os IDs dos produtos sejam únicos.
+
 SELECT DISTINCT id_produto FROM tabelavendasdesafioaula1;
 
+#2. Identifique os clientes que se cadastraram na empresa antes de 2020. Liste o nome e a data de cadastro desses clientes. Utilize a cláusula WHERE para filtrar os registros.
+
 SELECT nome_do_cliente FROM tabelaclientesdesafioaula1 WHERE data_de_cadastro < '2020-01-01';
+

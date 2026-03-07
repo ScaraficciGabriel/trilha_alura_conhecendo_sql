cf371e63c03b6ec327e7b4efbb72578f84850355
Gabriel Scaraficci <gabriel.scaraficci@gmail.com>
2026-03-07 10:49:01 -0300

    Update mnm_a5_2.sql

--- conhecendo_sql/mnm_a5_2.sql
+++ conhecendo_sql/mnm_a5_2.sql
@@ -1,3 +1,27 @@
+#1. Criar tabelas 
+  
+O primeiro passo é definir a estrutura do banco de dados, criando tabelas para armazenar informações específicas. Utilizaremos SQL (Structured Query Language) para criar as tabelas. Aqui estão os dados que você pode utilizar:
+
+#Tabela "Alunos" armazenará informações sobre os estudantes, como:
+
+#ID do Aluno: Um identificador único para cada aluno. Nome do Aluno: O nome completo do aluno. Data de Nascimento: A data de nascimento do aluno. Gênero: O gênero do aluno (masculino, feminino, outros). Endereço: O endereço do aluno. Telefone de Contato: O número de telefone de contato do aluno. E-mail: O endereço de e-mail do aluno. Tabela "Professores" conterá detalhes sobre os professores da escola:
+
+#ID do Professor: Um identificador único para cada professor. Nome do Professor: O nome completo do professor. Data de Nascimento: A data de nascimento do professor. Gênero: O gênero do professor. Telefone de Contato: O número de telefone de contato do professor. E-mail: O endereço de e-mail do professor. Tabela "Disciplinas" manterá registros das matérias oferecidas, incluindo:
+
+#ID da Disciplina: Um identificador único para cada disciplina. Nome da Disciplina: O nome da disciplina. Descrição: Uma descrição da disciplina. Carga Horária: A carga horária da disciplina. ID do Professor: Uma chave estrangeira que faz referência ao professor que leciona a disciplina. Tabela "Turmas" será usada para registrar turmas específicas:
+
+#ID da Turma: Um identificador único para cada turma. Nome da Turma: O nome ou código da turma. Ano Letivo: O ano letivo da turma. ID do Professor Orientador: Uma chave estrangeira que faz referência ao professor que orienta a turma. Tabela "Turma_Disciplinas" armazenara os dados das associações entre turmas e disciplinas:
+
+#ID da Turma: Uma lista de chaves estrangeiras que fazem referência as turmas existentes. ID das Disciplinas: Uma lista de chaves estrangeiras que fazem referência às disciplinas ministradas na turma. OBS.: Os dois campos juntos formam a chave primaria da tabela
+
+#Tabela "Turma_Alunos" será usada para registrar os dados das associações entre turmas e alunos:
+
+#ID da Turma: Uma lista de chaves estrangeiras que fazem referência as turmas existentes. ID dos Alunos: Uma lista de chaves estrangeiras que fazem referência aos alunos matriculados na turma. OBS.: Os dois campos juntos formam a chave primaria da tabela
+
+#Tabela "Notas" guardará as notas dos alunos em diferentes disciplinas:
+
+#ID da Nota: Um identificador único para cada nota. ID do Aluno: Uma chave estrangeira que faz referência ao aluno. ID da Disciplina: Uma chave estrangeira que faz referência à disciplina. Valor da Nota: A nota atribuída ao aluno na disciplina. Data da Avaliação: A data em que a avaliação foi realizada. 
+  
 CREATE TABLE alunos (
   id_aluno			INT PRIMARY KEY,
   nome_aluno		VARCHAR(100),
@@ -63,6 +87,10 @@ CREATE TABLE notas (
   FOREIGN KEY (id_aluno) REFERENCES alunos(id_aluno),
   FOREIGN KEY (id_disciplina) REFERENCES disciplina(id_disciplina)
 );
+
+#2 - Inserir dados
+
+#Depois de criar as tabelas, é hora de inserir os dados. Para isso, você pode baixar aqui neste link (arquivo-desafio), todos os arquivos com os dados para popular todas as tabelas.
 
 INSERT INTO 'alunos' ('id_aluno','nome_aluno','data_nasc_aluno','genero_aluno','endereco_aluno','tel_contato_aluno','email_aluno') VALUES 
  ('1','João Silva','2005-03-15','Masculino','Rua das Flores, 123','(11) 9876-5432','joao@email.com'), 
@@ -180,6 +208,12 @@ INSERT INTO 'notas' (id_nota, id_aluno, id_disciplina, valor_nota, data_avaliaca
 (50,5,9,4.24,'08/15/2023'),
 (51,5,10,0.11,'08/15/2023');
 
+#3 - Realizar consultas simples
+
+#Com os dados inseridos nas tabelas, podemos realizar consultas simples para obter informações úteis, como:
+
+#Consulta 1: Executar consultas para verificar se os dados foram importados corretamente em todas as tabelas.
+
 SELECT * FROM alunos;
 SELECT * FROM disciplina;
 SELECT * FROM notas;
@@ -188,9 +222,16 @@ SELECT * FROM turmas;
 SELECT * FROM turma_alunos;
 SELECT * FROM turma_disciplinas;
 
+#Consulta 2: Retorne as informações de todos os alunos ordenados pelo nome.
+
 SELECT * FROM alunos ORDER BY nome_aluno;
 
+#Consulta 3: Retornar a disciplina que possui a carga horaria maior que 40.
+
 SELECT * FROM disciplina WHERE carga_horaria > 40;
+  
+#Consulta 4: Buscar as notas que são maiores que 6 e menores que 8.
 
 SELECT * FROM notas WHERE valor_nota > 6 AND valor_nota < 8;
+
 

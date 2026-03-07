72086c53c8269564d88901d9cee37334fa5576e0
Gabriel Scaraficci <gabriel.scaraficci@gmail.com>
2026-03-07 10:21:36 -0300

    Create employees table and insert sample data
    Added SQL instructions to create and insert into the employees table.

--- conhecendo_sql/mnm_a3.sql
+++ conhecendo_sql/mnm_a3.sql
@@ -1,3 +1,7 @@
+#Você trabalha como um administrador de banco de dados em uma empresa de recursos humanos. A empresa precisa armazenar informações sobre novos funcionários em seu banco de dados. Sua tarefa é criar uma instrução SQL INSERT para inserir os detalhes de um novo funcionário em uma tabela de funcionários.
+
+#1. A tabela de funcionários possui os seguintes campos: ID (Chave Primária) Nome (Texto) Cargo (Texto) Departamento (Texto) Data de Contratação (Data) Salário (Decimal).
+
 CREATE TABLE tabela_funcionarios (
   id_funcionario		INT	PRIMARY KEY,
   nome_funcionario			VARCHAR(100),
@@ -7,7 +11,10 @@ CREATE TABLE tabela_funcionarios (
   salario_funcionario			FLOAT
   );
 
+#2. Escreva uma instrução SQL INSERT para adicionar um novo funcionário à tabela. Use os seguintes dados de exemplo: ID: 32D Nome: João Silva Cargo: Desenvolvedor de Software Departamento: TI Data de Contratação: 2023-10-24 Salário: 6000.00
+
 INSERT INTO tabela_funcionarios
 (id_funcionario, nome_funcionario, cargo_funcionario, departamento, data_contratacao, salario_funcionario)
 VALUES
+
 ('32D', 'João Silva', 'Desenvolvedor Software', 'TI', '2023-10-24', 6000.00);

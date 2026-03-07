976f004a8ff853556067101f3a78c54ccc4d509e
Gabriel Scaraficci <gabriel.scaraficci@gmail.com>
2026-03-07 10:18:54 -0300

    Reformat SQL queries with added context

--- conhecendo_sql/mnm_a1.sql
+++ conhecendo_sql/mnm_a1.sql
@@ -1,3 +1,5 @@
+#Você é uma pessoa analista de dados em uma empresa de comércio eletrônico e foi escolhido dentro da equipe para analisar o banco de dados de vendas da empresa. Você precisa extrair informações específicas sobre os clientes e os produtos vendidos.
+
 #1. Liste todos os IDs únicos dos produtos vendidos pela empresa, ou seja, uma lista de produtos distintos. Utilize a cláusula DISTINCT para garantir que os IDs dos produtos sejam únicos.
 
 SELECT DISTINCT id_produto FROM tabelavendasdesafioaula1;
@@ -5,4 +7,5 @@ SELECT DISTINCT id_produto FROM tabelavendasdesafioaula1;
 #2. Identifique os clientes que se cadastraram na empresa antes de 2020. Liste o nome e a data de cadastro desses clientes. Utilize a cláusula WHERE para filtrar os registros.
 
 SELECT nome_do_cliente FROM tabelaclientesdesafioaula1 WHERE data_de_cadastro < '2020-01-01';
+
 
